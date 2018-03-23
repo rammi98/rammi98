e083e9e23bdf3faea62915fa310b2e513fb36c30
rammi98 <35140861+rammi98@users.noreply.github.com>
2018-03-23 23:53:25 +0530

    Update hel.c

--- hel.c
+++ hel.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int i,n;
+int i,n,rev=0,rem,t;
 scanf("%d",&n);
-for(i=1;i<=n;i++)
-printf("hello\n");
+  t=n;
+while(n!=0)
+  {
+  rem=n%10;
+  n=n/10;
+  rev=(rev*10)+rem;}
+  {
+  if(rev==t)
+printf("yes");
+    else
+      printf("no");
+  }
 return 0;
 }
