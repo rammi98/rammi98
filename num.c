76bc2bb23a357514fb35d2ae62bb69574e1aa627
rammi98 <35140861+rammi98@users.noreply.github.com>
2018-03-23 23:19:04 +0530

    Update num.c

--- num.c
+++ num.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int n,count=0;
+int number,count=0;
 scanf("%d",&n);
-while(n!=0)
+while(number!=0)
 {
-n=n/10;
+number=number/10;
 count++;
 }
 printf("%d",count);
