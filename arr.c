9166c7945d0b2bcebe83093863afa757d34bfbc1
rammi98 <35140861+rammi98@users.noreply.github.com>
2018-07-10 20:14:22 +0530

    Create arr.c

---
+++ arr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[100];
+int n1,n2,sum,i;
+scanf("%d""%d",&n1,&n2);
+for(i=0;i<n1;i++)
+{
+scanf("%d",&a[i]);
+}
+sum=a[0]+a[1];
+{
+printf("%d",sum);
+}
+return 0;
+}
