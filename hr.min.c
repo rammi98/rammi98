e90e1310a6a339580e2638ad82a0d25bf3a4a7b5
rammi98 <35140861+rammi98@users.noreply.github.com>
2018-08-24 00:00:01 +0530

    Update hr.min.c

--- hr.min.c
+++ hr.min.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,d;
+int a,b,c,d,t,t1;
 scanf("%d%d%d%d",&a,&b,&c,&d);
-int t=a-c;
-int t1=b-d;
+  if(a>c)
+t=a-c;
+  else
+    t=c-a;
+  if(b>d)
+t1=b-d;
+  else
+  t1=d-b;  
+  
 printf("%d %d",t,t1);
 }
